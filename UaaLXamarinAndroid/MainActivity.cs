577f43d8ce386bd69312e7d89440ca7f3ebe0c16
f-miyu <matsushima23@gmail.com>
2020-12-08 00:36:30 +0900

    初期コミット

---
+++ UaaLXamarinAndroid/MainActivity.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace UaaLXamarinAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private bool isUnityLoaded = false;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            HandleIntent(Intent);
+
+            FindViewById<Button>(Resource.Id.button).Click += (_, __) =>
+            {
+                //  Load
+                isUnityLoaded = true;
+                var intent = new Intent(this, typeof(MainUnityActivity));
+                intent.SetFlags(ActivityFlags.ReorderToFront);
+                StartActivityForResult(intent, 1);
+                //StartActivity(typeof(MainUnityActivity));
+            };
+
+            FindViewById<Button>(Resource.Id.button2).Click += (_, __) =>
+            {
+                //  Unload
+                UnloadUnity(true);
+            };
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            HandleIntent(intent);
+            Intent = intent;
+        }
+
+        private void HandleIntent(Intent intent)
+        {
+            if (intent == null || intent.Extras == null)
+            {
+                return;
+            }
+
+            if (intent.Extras.ContainsKey("setColor"))
+            {
+                View v = FindViewById(Resource.Id.button2);
+                switch (intent.Extras.GetString("setColor"))
+                {
+                    case "yellow": v.SetBackgroundColor(Color.Yellow); break;
+                    case "red": v.SetBackgroundColor(Color.Red); break;
+                    case "blue": v.SetBackgroundColor(Color.Blue); break;
+                    default: break;
+                }
+            }
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (requestCode == 1)
+            {
+                isUnityLoaded = false;
+            }
+        }
+        public void UnloadUnity(bool doShowToast)
+        {
+            if (isUnityLoaded)
+            {
+                var intent = new Intent(this, typeof(MainUnityActivity));
+                intent.SetFlags(ActivityFlags.ReorderToFront);
+                intent.PutExtra("doQuit", true);
+                StartActivity(intent);
+                isUnityLoaded = false;
+            }
+            else if (doShowToast)
+            {
+                ShowToast("Show Unity First");
+            }
+        }
+
+        public void ShowToast(string message)
+        {
+            var toast = Toast.MakeText(ApplicationContext, message, ToastLength.Short);
+            toast.Show();
+        }
+
+        public override void OnBackPressed()
+        {
+            FinishAffinity();
+        }
+    }
+}
---
+++ UaaLXamarinAndroid/MainUnityActivity.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Company.Product;
+using Com.Unity3d.Player;
+
+namespace UaaLXamarinAndroid
+{
+    [Activity(
+        Name = "uaalXamarinAndroid.MainUnityActivity",
+        Label = "@string/app_name",
+        Process = ":Unity",
+        ScreenOrientation = ScreenOrientation.FullSensor,
+        LaunchMode = LaunchMode.SingleTask,
+        HardwareAccelerated = false,
+        ConfigurationChanges = ConfigChanges.Mcc |
+                               ConfigChanges.Mnc |
+                               ConfigChanges.Locale |
+                               ConfigChanges.UiMode |
+                               ConfigChanges.Density |
+                               ConfigChanges.Keyboard |
+                               ConfigChanges.FontScale |
+                               ConfigChanges.ScreenSize |
+                               ConfigChanges.Navigation |
+                               ConfigChanges.Touchscreen |
+                               ConfigChanges.Orientation |
+                               ConfigChanges.ScreenLayout |
+                               ConfigChanges.KeyboardHidden |
+                               ConfigChanges.LayoutDirection |
+                               ConfigChanges.SmallestScreenSize)]
+    public class MainUnityActivity : OverrideUnityActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            AddControlsToUnityFrame();
+            HandleIntent(Intent);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            HandleIntent(intent);
+            Intent = intent;
+        }
+
+        private void HandleIntent(Intent intent)
+        {
+            if (intent == null || intent.Extras == null)
+            {
+                return;
+            }
+
+            if (intent.Extras.ContainsKey("doQuit"))
+            {
+                if (MUnityPlayer != null)
+                {
+                    Finish();
+                }
+            }
+        }
+
+        protected override void ShowMainActivity(string setToColor)
+        {
+            var intent = new Intent(this, typeof(MainUnityActivity));
+            intent.SetFlags(ActivityFlags.ReorderToFront | ActivityFlags.SingleTop);
+            intent.PutExtra("setColor", setToColor);
+            StartActivity(intent);
+        }
+
+        public override void OnUnityPlayerUnloaded()
+        {
+            ShowMainActivity("");
+        }
+
+        public void AddControlsToUnityFrame()
+        {
+            FrameLayout layout = MUnityPlayer;
+
+            {
+                Button myButton = new Button(this);
+                myButton.Text = "Show Main";
+                myButton.SetX(10);
+                myButton.SetY(500);
+                myButton.Click += (_, __) =>
+                {
+                    ShowMainActivity("");
+                };
+            }
+
+            {
+                Button myButton = new Button(this);
+                myButton.Text = "Send Msg";
+                myButton.SetX(320);
+                myButton.SetY(500);
+                myButton.Click += (_, __) =>
+                {
+                    UnityPlayer.UnitySendMessage("Cube", "ChangeColor", "yellow");
+                };
+                layout.AddView(myButton, 300, 200);
+            }
+
+            {
+                Button myButton = new Button(this);
+                myButton.Text = "Unload";
+                myButton.SetX(630);
+                myButton.SetY(500);
+
+                myButton.Click += (_, __) =>
+                {
+                    MUnityPlayer.Unload();
+                };
+                layout.AddView(myButton, 300, 200);
+            }
+
+            {
+                Button myButton = new Button(this);
+                myButton.Text = "Finish";
+                myButton.SetX(630);
+                myButton.SetY(800);
+
+                myButton.Click += (_, __) =>
+                {
+                    Finish();
+                };
+                layout.AddView(myButton, 300, 200);
+            }
+        }
+
+    }
+}
